eed166f02edbcd899309dd18e7bb51d27ff02351
Pablo Santos <opablosantos@gmail.com>
2021-07-17 20:43:47 -0300

    Testando o javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+var myName = "Pablo"
+
+var age = 27
+
+var height = 1.76
+
+var person = {
+    "name": "Pablo",
+    "age": 27
+}
+
+var students = ["Pablo", "Joao", "Pedro"]
+
+function sum(a,b) {
+    return a+b
+}
